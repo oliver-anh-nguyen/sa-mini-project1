5265556c5c05bc2047465688fca7303c82719ea0
Phat Nguyen <phatnguyentanit@gmail.com>
2022-10-08 18:00:45 -0500

    Cleaned up database right after exiting application

---
+++ src/main/java/edu/miu/sa/miniproject1/configuration/ApplicationConfiguration.java
@@ -0,0 +1,29 @@
+package edu.miu.sa.miniproject1.configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.annotation.PreDestroy;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class ApplicationConfiguration {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @PreDestroy
+    public void onExit() {
+        log.info("Cleaning up database....");
+        jdbcTemplate.execute("DELETE FROM student");
+        jdbcTemplate.execute("DELETE FROM batch_job_execution_params");
+        jdbcTemplate.execute("DELETE FROM batch_job_execution_context");
+        jdbcTemplate.execute("DELETE FROM batch_step_execution_context");
+        jdbcTemplate.execute("DELETE FROM batch_step_execution");
+        jdbcTemplate.execute("DELETE FROM batch_job_execution");
+        jdbcTemplate.execute("DELETE FROM batch_job_instance");
+        log.info("Database cleaned!");
+    }
+}
